223fd504b753c908dd72d8e3db2633521d226955
Felix Alm <felix.kauppi.alm@gmail.com>
2024-12-03 09:53:55 +0100

    Add edit buttons to trades

--- lib/elixir_xtdb_web/live/trades.ex
+++ lib/elixir_xtdb_web/live/trades.ex
@@ -33,7 +33,6 @@ defmodule ElixirXtdbWeb.Trades do
     |> assign(:editing_trade, trade)
   end
 
-
   def update_system_time_slider_state(socket, index, transactions) do
     socket
     |> assign(:system_from_index, index)
@@ -58,11 +57,23 @@ defmodule ElixirXtdbWeb.Trades do
 
   def trade(assigns) do
     ~H"""
-    <div
-      class="flex items-center gap-2 cursor-pointer"
-      phx-click="edit_trade"
-      phx-value-id={@trade._id}
-    >
+    <div class="flex items-center gap-2">
+      <div class="cursor-pointer" phx-click="edit_trade" phx-value-id={@trade._id}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="icon icon-tabler icons-tabler-outline icon-tabler-edit"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" />
+        </svg>
+      </div>
       <span class="font-semibold"><%= @trade._id %>:</span>
       <code><%= @trade.symbol %></code>
       | <code><%= @trade.volume %></code>
@@ -157,7 +168,7 @@ defmodule ElixirXtdbWeb.Trades do
 
     socket =
       socket
-      |> update_system_time_slider_state(system_from_index,  socket.assigns.transactions)
+      |> update_system_time_slider_state(system_from_index, socket.assigns.transactions)
       |> assign(:trades, trades)
 
     {:noreply, socket}
